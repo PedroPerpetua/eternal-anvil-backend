5e5f43d857738b3d4873c7d823a8007bea09272a
Pedro Perpétua <46409254+PedroPerpetua@users.noreply.github.com>
2024-06-03 10:26:51 +0100

    Initialize develop

---
+++ app/core/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+
+class TestCoreAPI(APITestCase):
+    """Test the Core API endpoints."""
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_ping(self) -> None:
+        """Test the ping endpoint."""
+        res = self.client.get(reverse("ping"))
+        self.assertEqual(status.HTTP_200_OK, res.status_code)
+        self.assertEqual("pong", res.json())
---
+++ app/users/authentication.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from django.contrib.auth.backends import ModelBackend
+from rest_framework.permissions import IsAuthenticated as BaseIsAuthenticated
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from users.models import User
+
+
+class AuthenticationBackend(ModelBackend):
+    """Custom authentication backend that also checks for the `is_deleted` status."""
+
+    def user_can_authenticate(self, user: Optional[User]) -> bool:  # type: ignore[override] # Use our user model
+        if user and user.is_deleted:
+            return False
+        return super().user_can_authenticate(user)
+
+
+class AuthenticatedRequest(Request):
+    """Authenticated class to correctly type the user in requests."""
+
+    user: User
+
+
+class IsAuthenticated(BaseIsAuthenticated):
+    """Modify the IsAuthenticated permission to block inactive and deleted users."""
+
+    def has_permission(self, request: AuthenticatedRequest, view: APIView) -> bool:  # type: ignore[override]
+        return super().has_permission(request, view) and request.user.is_active and not request.user.is_deleted
+
+
+class IsStaff(IsAuthenticated):
+    """Extend the `IsAuthenticated` permission to only allow users with `is_staff == True`."""
+
+    def has_permission(self, request: AuthenticatedRequest, view: APIView) -> bool:  # type: ignore[override]
+        return super().has_permission(request, view) and request.user.is_staff
